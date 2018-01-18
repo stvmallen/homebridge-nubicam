8f35ba66c18ed39a90cd6857e51456518f31ac7b
Esteban Mallen <esteban-mallen@users.noreply.github.com>
2018-01-18 11:14:24 -0300

    First working version (#1)

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # homebridge-nubicam
+homebridge-plugin for Nubicam (www.nubicam.com.ar)
+
+[![npm version](https://badge.fury.io/js/homebridge-nubicam.svg)](https://badge.fury.io/js/homebridge-roomba-stv)
+[![dependencies Status](https://david-dm.org/esteban-mallen/homebridge-nubicam/status.svg)](https://david-dm.org/esteban-mallen/homebridge-nubicam)
+
+### Credits:
+
+- @KhaosT for HAP and the FFMPEG plugin
+- @nfarina for homebridge
+- @nayrnet for the hikvision module
+
+### Features:
+
+- Camera live feed in Home.app
+- Camera snapshots en Home.app favorites
+- Motion sensor
+
+## Installation:
+
+### 1. Install homebridge and Roomba plugin.
+- 1.1 `npm install -g homebridge`
+- 1.2 `npm install -g homebridge-nubicam`
+
+### 2. Update homebridge configuration file.
+```
+{
+  "platform" : "Nubicam",
+  "name" : "Nubicam",
+  "username" : "username@test.com",
+  "password" : "123456"
+}
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+let Accessory, hap;
+let ubiguardAPI = require('./lib/ubiguard');
+let CameraAccessory = require('./lib/Camera').Camera;
+let WaitUntil = require('wait-until');
+
+module.exports = function (homebridge) {
+    Accessory = homebridge.platformAccessory;
+    hap = homebridge.hap;
+
+    homebridge.registerPlatform("homebridge-nubicam", "Nubicam", nubicam, true);
+};
+
+const nubicam = function (log, config, api) {
+    this.log = log;
+    this.api = api;
+
+    this.nubicamUsername = config.username;
+    this.nubicamPassword = config.password;
+    this.cameraAccessories = [];
+
+    if (!api || api.version < 2.1) {
+        throw new Error('Unexpected API version.')
+    }
+
+    api.on('didFinishLaunching', this.didFinishLaunching.bind(this))
+};
+
+nubicam.prototype = {
+    configureAccessory(accessory) {
+        this.log("configureAccessory() invoked! %s", accessory);
+    },
+
+    didFinishLaunching() {
+        let platform = this;
+
+        let user = new ubiguardAPI.User(this.nubicamUsername, this.nubicamPassword);
+
+        user.login()
+            .then(() => {
+                platform.log.debug("Nubicam User ID = %s", user.userId);
+
+                user.getCameras()
+                    .then((cameras) => {
+                        platform.log("Found %s cameras", cameras.length);
+
+                        cameras.forEach(camera => {
+                            platform.cameraAccessories.push(new CameraAccessory(new ubiguardAPI.Camera(user, camera), platform.log, Accessory, hap));
+                        });
+
+                        WaitUntil()
+                            .interval(500)
+                            .times(20)
+                            .condition(function () {
+                                return platform.cameraAccessories.every(camera => camera.isInitialized);
+                            })
+                            .done((allCamerasInitialized) => {
+                                if (!allCamerasInitialized) {
+                                    platform.log.warn("Some cameras are not fully initialized");
+                                }
+
+                                platform.api.publishCameraAccessories("Nubicam", platform.cameraAccessories.map(camera => camera.getAccessory()));
+                            });
+                    }).catch(reason => platform.log.error(reason));
+            }).catch(reason => platform.log.error(reason));
+    }
+};
